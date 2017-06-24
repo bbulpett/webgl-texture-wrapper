943f345a2190786c99d217e32e623bdf6ac8fe45
Barnabas Bulpett <barnabasbulpett@gmail.com>
2017-06-24 12:48:52 -0400

    Buffers for initial object

---
+++ js/object.js
@@ -0,0 +1,15 @@
+var horizAspect = 480.0/640.0;
+
+function initBuffers() {
+  squareVerticesBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, squareVerticesBuffer);
+
+  var vertices = [
+    1.0, 1.0, 0.0,
+    -1.0, 1.0, 0.0,
+    1.0, -1.0, 0.0,
+    -1.0, -1.0, 0.0
+  ];
+
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+}
