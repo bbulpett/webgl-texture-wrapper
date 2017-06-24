0aca8a7b5162592bb91e9552873682a5765eb5bc
Barnabas Bulpett <barnabasbulpett@gmail.com>
2017-06-24 12:22:50 -0400

    Add shader scripts to page

--- js/shaders.js
+++ js/shaders.js
@@ -18,3 +18,36 @@ function initShaders() {
   vertexPositionAttribute = gl.getAttribLocation(shaderProgram, 'aVertexPosition');
   gl.enableVertexAttribArray(vertexPositionAttribute);
 }
+
+// The getShader() routine fetches shader program from the DOM
+function getShader(gl, id, type) {
+  var shaderScript, theSource, currentChild, shader;
+  shaderScript = document.getElementById(id);
+
+  if (!shaderScript) {
+    return null;
+  }
+  theSource = shaderScript.text;
+
+  if (!type) {
+    if (shaderScript.type == 'x-shader/x-fragment') {
+      type = gl.FRAGMENT_SHADER;
+    } else if (shaderScript.type == 'x-shader/x-vertex') {
+      type = gl.VERTEX_SHADER;
+    } else {
+      return null; // Shader type is unknown
+    }
+  }
+  shader = gl.createShader(type);
+
+  gl.shaderSource(shader, theSource);
+
+  gl.compileShader(shader); // Compile the shader program
+  // Check that it compiled successfully
+  if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {  
+      console.log('An error occurred compiling the shaders: ' + gl.getShaderInfoLog(shader));  
+      gl.deleteShader(shader);
+      return null;  
+  }
+  return shader;
+}
